fccb9033a1158c699be2648946bcd2594c859534
junho.hwang <junho.hwang@navercorp.com>
2018-02-05 12:24:04 +0900

    java scala 연동 예제

---
+++ src/main/java/com/java/example/JavaExampleUtil.java
@@ -0,0 +1,8 @@
+package com.java.example;
+
+public class JavaExampleUtil {
+
+    public static void printMessage() {
+        System.out.println(">>>>>>>>>>>>>>>>>> Test Message");
+    }
+}
--- src/main/scala/com/scala/example/SparkExampleApp.scala
+++ src/main/scala/com/scala/example/SparkExampleApp.scala
@@ -1,10 +1,13 @@
 package com.scala.example
 
+import com.java.example.JavaExampleUtil
 import org.apache.spark.{SparkConf, SparkContext}
 
 object SparkExampleApp {
 
   def main(args: Array[String]) {
+
+    JavaExampleUtil.printMessage()
 
     val conf = new SparkConf().setAppName("SparkJoins").setMaster("local")
     val context = new SparkContext(conf)
